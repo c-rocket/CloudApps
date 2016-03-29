861bb47077a985c63359dde7fc3b682502d1a61d
ccrock <cam.crockett.12@gmail.com>
2016-03-29 08:08:03 -0600

    Removing Angular front end

--- IotGatewaySimulator/src/main/webapp/resources/js/app/Service/DeviceListService.js
+++
@@ -1,8 +0,0 @@
-app.factory('DeviceListService', function($http) {
-	var deviceListService = {
-		getDevices : function() {
-			return $http.get(baseUrl + '/device/types');
-		}
-	}
-	return deviceListService;
-});
